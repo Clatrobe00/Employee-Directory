8b08e3b1e3935dc004db024295954e57598bd345
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-03-30 13:37:43 -0700

    add api response to page using react Component

--- src/components/SearchHandler.js
+++ src/components/SearchHandler.js
@@ -1,13 +1,26 @@
+import React, { Component } from 'react';
 import API from './utils/API';
 
-const SearchHandler = async () => {
-    const res = await API();
-    console.log(res.data.results);
-    return (
-        <div>
-            {res.data.results}
-        </div>
-    )
+class SearchHandler extends Component {
+    state = {
+        employees: [],
+    }
+
+    componentDidMount () {
+        API().then(res => {
+            const employees = res.data.results;
+            this.setState({ employees })
+        })
+    }
+
+    render() {
+        return (
+          <ul>
+              Hello
+            { this.state.employees.map(employee => <li>{employee.name.first}</li>)}
+          </ul>
+        )
+      }
 }
 
 export default SearchHandler;
--- src/components/utils/API.js
+++ src/components/utils/API.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const URL = 'https://randomuser.me/api/?results=5';
 
-const API = () =>  axios.get(URL);
+const API = () => axios.get(URL);    
 
 export default API;
