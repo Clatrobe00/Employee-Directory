dfd5c40f14397d3bc1705c77a134366cded5bb53
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-03 22:35:36 -0700

    add userSearch state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,22 @@
+import { useState } from 'react'
 import Form from 'react-bootstrap/Form';
 
-
 const SearchBar = () => {
+
+    const [userSearch, setUserSearch] = useState({
+        name: "name"
+    });
+
+    const handleChange = (e) => {
+        setUserSearch({name: e.target.value});
+        console.log('target data is ', e.target.value);
+    }
 
     return (
         <Form>
         <Form.Group controlId="formBasicEmail">
             <Form.Label>Search for Employee</Form.Label>
-            <Form.Control type="searchBar" placeholder="Enter name" />
+            <Form.Control type="text" value={userSearch.name} onChange={handleChange}/>
             <Form.Text className="text-muted">
             Just type in the first name of the employee you need.
             </Form.Text>
